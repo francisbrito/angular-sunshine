0e49ec3b01d0598763a4594ae202cbefc31bdce6
Francis Brito <fr.br94@gmail.com>
2015-04-14 00:19:51 -0400

    Add application router.

--- src/scripts/app/index.js
+++ src/scripts/app/index.js
@@ -1,5 +1,15 @@
 'use strict';
 
+var angular = require('angular'),
+    angularRoute = require('angular-route');
+
 module.exports = function init() {
-    console.log('Application initalized.');
+    console.log('Application initialized.');
+
+    return angular.module('sunshine', [angularRoute])
+    .config(($routeProvider) => {
+        $routeProvider.when('/', {
+            template: '<h1>It works!</h1>'
+        });
+    });
 };
